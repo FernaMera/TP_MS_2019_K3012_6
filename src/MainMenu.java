1e154a00a74ddb7a7f866d7c342c32428f554f9f
fernamera <f3rna@live.com.ar>
2019-04-15 18:54:25 -0300

    Interfaz Grafica en JavaFX

---
+++ src/MainMenu.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainMenu extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        Parent root = loader.load(getClass().getResource("MainMenu.fxml"));
+        primaryStage.setTitle("NCOM");
+        primaryStage.setScene(new Scene(root, 287, 255));
+        primaryStage.show();
+    }
+
+}
