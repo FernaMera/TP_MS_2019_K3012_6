4c3d07e0c3992c5e4d92c6200024ab5e1f3971a4
noeliaescalier <31080603+noeliaescalier@users.noreply.github.com>
2019-04-22 18:19:28 -0300

    Agrego caso en el que el numero real vale 0 y uso de DecimalFormat

--- src/NumeroComplejoBinomica.java
+++ src/NumeroComplejoBinomica.java
@@ -13,24 +13,39 @@ public class NumeroComplejoBinomica {
         double modulo = Math.sqrt(Math.pow(real, 2) + Math.pow(imaginario, 2));
         double argumento = Math.atan(imaginario/real) / Math.PI;
         
+         
+        if(real == 0 && imaginario >= 1){
+            argumento = 0.5;
+        }
+        
+        if(real == 0 && imaginario <= -1){
+            argumento = 0.75;
+        }
+        
+        //2do Cuadrante
         if(real < 0 && imaginario > 0)
         {
             //sumar PI
             argumento++;
         }
+        
+        // 3er Cuadrante
         if(real < 0 && imaginario < 0)
         {
             //sumar PI
             argumento++;
         }
+        
+        //4to Cuadrante
         if(real > 0 && imaginario < 0)
         {
             //sumar 2PI
             argumento += 2;
         }
         
+        DecimalFormat df = new DecimalFormat("0.00");
         
-        System.out.println("[" + modulo + ";" + argumento + "PI" + "]");
-        return "[" + modulo + ";" + argumento + "PI" + "]";
+        System.out.println("[" + df.format(modulo) + ";" + df.format(argumento) + "PI" + "]");
+        return "[" + df.format(modulo) + ";" +df.format (argumento) + "PI" + "]" ;
     }
 }
