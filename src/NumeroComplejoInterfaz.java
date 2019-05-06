a4b8273aeb113c14034796c5dd53e637dc094d1f
fernamera <f3rna@live.com.ar>
2019-05-06 14:23:43 -0300

    agregada intrefaz

---
+++ src/NumeroComplejoInterfaz.java
@@ -0,0 +1,10 @@
+public interface NumeroComplejoInterfaz {
+    String sumar(NumeroComplejoBinomica x);
+    String sumar(NumeroComplejoPolar x);
+    String restar(NumeroComplejoBinomica x);
+    String restar(NumeroComplejoPolar x);
+    String multiplicar(NumeroComplejoBinomica x);
+    String multiplicar(NumeroComplejoPolar x);
+    String dividir(NumeroComplejoBinomica x);
+    String dividir(NumeroComplejoPolar x);
+}
