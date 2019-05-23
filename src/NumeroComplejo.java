d2a3717fa5cdd48e20fe74d9d9a86f4dee2d18bf
noeliaescalier <31080603+noeliaescalier@users.noreply.github.com>
2019-05-23 12:42:05 -0300

    agrego mostrar resultado en NumeroComplejo para fasor

--- src/NumeroComplejo.java
+++ src/NumeroComplejo.java
@@ -9,4 +9,11 @@ public abstract class NumeroComplejo
         System.out.println( abrirPar + df.format(a) + ";" +  df.format(b) + cerrarPar);
         return abrirPar + df.format(a) + ";" + df.format(b) + cerrarPar ;
     }
+    // para poder mostrar los fasores
+     public String mostrarResultado(double amplitud, String funcion ,double frecuencia ,double fase ,String numeroPi  ){
+        
+        DecimalFormat d = new DecimalFormat("0.00");
+         System.out.println( d.format(amplitud) + " " + funcion + "(" + d.format(frecuencia) +"t" + d.format(fase) + numeroPi + ")");
+        return d.format(amplitud) + funcion + "(" + d.format(frecuencia) + "t"  + d.format(fase) +numeroPi + ")" ;
+    }
 }
